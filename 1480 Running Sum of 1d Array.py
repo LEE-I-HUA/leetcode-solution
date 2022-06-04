6ef629b2e6d2a04e9cf9e129ae22ebc345e0bd29
LEE-I-HUA <93990513+LEE-I-HUA@users.noreply.github.com>
2022-06-04 17:23:25 +0800

    Update 1480 Running Sum of 1d Array.py

--- 1480 Running Sum of 1d Array.py
+++ 1480 Running Sum of 1d Array.py
@@ -1,7 +1,7 @@
 # Input: nums = [1,2,3,4]
 # Output: [1,3,6,10]
 # Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
-
+# solution 1
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         answer=[]
@@ -9,4 +9,15 @@ class Solution:
         for i in  nums:
             sumi += i  
             answer.append(sumi)
+        return answer
+    
+# solution 2    
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        answer=[]
+        for i in range (len(nums)): 
+            if i ==0:
+                answer.append(nums[i])
+            else:
+                answer.append(nums[i]+answer[i-1])
         return answer
